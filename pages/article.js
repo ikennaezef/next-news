7db3cb121a6e4e6fed1f83d5e71e00e5e952f4f6
Ikenna <iykee0333@gmail.com>
2022-05-09 13:12:15 +0100

    updated article page

--- pages/article.js
+++ pages/article.js
@@ -1,4 +1,5 @@
 import { useApp } from '../context/state';
+import Link from 'next/link';
 import styles from '../styles/Article.module.css';
 
 const article = () => {
@@ -8,7 +9,7 @@ const article = () => {
 
   return (
     <div>
-      {currentArticle && (
+      {currentArticle ? (
         <>
           <h2 className={styles.title}>{currentArticle.title}</h2>
           <img src={currentArticle?.urlToImage} className={styles.image} />
@@ -16,6 +17,13 @@ const article = () => {
           <p className={styles.author}>Source: {currentArticle?.source?.name}</p>
           <p>Vengo baile la recodos mudas lo la la la desgarrados, lo quemadas hombrecillo húmedo me olvidando mármol los tierra talco. El nino sus que las algodón el las lenguas muertos. Los queman atrás transparente de donde tránsito las y nino. Deja brooklyn tierra los olvidando. Con pies amor mueven diana de entonces las, vacía tierra mi sepultura mueven huevos para los se yo. Que subía la nube la los pero de el tierra, tierra amor por perfil de de, pero.</p>
           <a href={currentArticle.url} target='_blank' className={styles.link} >Read More</a>
+        </>
+      ) : (
+        <>
+          <p>Sorry, there is nothing here. Please return to the home page and select an article.</p>
+          <Link href='/'>
+            <a className={styles.link} >Return to Home</a>
+          </Link>
         </>
       )}
     </div>
